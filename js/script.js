2760da2552f733d87e18479082182b6abd311f8c
yukisako <yukisakoatoz@gmail.com>
2017-02-06 14:25:24 +0900

    add button

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 $(function(){
-  $('#password').keyup(function() {
+  $('#submit').click(function() {
     if($('#password').val()=="whydontyouchangetheworld"){
       window.location.href = 'top.html';
+    } else {
+      $(".input__label-content").text("incorrect");
     }
+  });
+  $('.input__field').click(function() {
+    $(".input__label-content").text("Secret Code");
   });
 });
